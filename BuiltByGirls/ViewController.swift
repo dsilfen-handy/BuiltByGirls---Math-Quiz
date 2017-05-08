15281f60545f33a191ee1779df5291dc79e8589b
Dean Silfen <dsilfen@handy.com>
2017-05-08 17:38:28 -0400

    Update storyboard, ViewController and README

--- BuiltByGirls/ViewController.swift
+++ BuiltByGirls/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var answerField: UITextField!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+//
+//  @IBAction func answerFieldChanged(_ sender: Any) {
+//    print("answer changed!")
+//    print(self.answerField.text ?? "")
+//  }
+  @IBAction func answerFieldEditingChanged(_ sender: Any) {
+        print("answer changed!")
+        print(self.answerField.text ?? "")
+  }
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
---
+++ README.md
@@ -0,0 +1,7 @@
+# To Get Started
+
+1) [Download Xcode](https://itunes.apple.com/us/app/xcode/id497799835?mt=12)
+2) Clone this repo `git clone ...`
+3) Open this project with Xcode
+4) Press play
+
